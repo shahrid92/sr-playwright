5d4c8b6dc5a59394ef0b4c90c16a6f3eee4eb774
Shah Ridzuan <repo.shahridzuan.aws@gmail.com>
2026-03-08 00:54:23 +0800

    ref: add html reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['json', {  outputFile: 'test-results.json' }]
+    ['json', {  outputFile: 'test-results.json' }],
+    ['html']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
