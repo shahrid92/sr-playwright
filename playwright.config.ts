d06e1f660cbaef53bac9356032c2e27ca258327d
Shah Ridzuan <repo.shahridzuan.aws@gmail.com>
2026-03-08 11:34:49 +0800

    ref: remove html reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,8 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['json', {  outputFile: 'test-results.json' }],
-    ['html']
+    ['json', {  outputFile: 'test-results.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
