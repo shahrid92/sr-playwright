ba32716b6b3bebd1e57e845e5d2454b694b07cda
Shah Ridzuan <repo.shahridzuan.aws@gmail.com>
2026-03-14 08:21:09 +0800

    feat: setup bullmq

---
+++ bull-mq/producer/generate-py-shards.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 1. Get total tests
+GET_TOTAL_TESTS=$(npx playwright test --grep @smoke --list --config '../../playwright.config.ts' --project='chromium')
+
+WORKERS=4
+
+# 2. Extract total test
+TOTAL_TESTS=$(echo $GET_TOTAL_TESTS | grep -Eo 'Total: [0-9]+' | grep -Eo '[0-9]+')
+
+SHARDS=$(( ($TOTAL_TESTS + $WORKERS - 1) / $WORKERS ))
+
+echo "Total Tests Discovered: $SHARDS"
+
